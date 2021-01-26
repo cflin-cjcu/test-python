1db252e37e2b483d4e13d887c8274267735fa021
cflin <cflin@mail.cjcu.edu.tw>
2021-01-26 14:48:02 +0800

    練習4-6

---
+++ 4-1.py
@@ -0,0 +1,5 @@
+a=int(input())
+b=int(input())
+# print(*range(a,b+1))
+for i in range(a,b-1,-1):
+    print(i,end=' ')
---
+++ 4-6.py
@@ -0,0 +1,5 @@
+sum=1
+n=int(input())
+for i in range(1,n+1):
+    sum *= i
+print(sum)    

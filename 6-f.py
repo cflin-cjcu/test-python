abea0ff5d69205d64b676eef093401086ea365a8
cflin <cflin@mail.cjcu.edu.tw>
2021-01-27 11:22:36 +0800

    練習6-f

--- 6-f.py
+++ 6-f.py
@@ -1,11 +1,10 @@
 n=int(input())
 a=1
-print(1,end=' ')
+# print(1,end=' ')
 b=1
-print(1,end=' ')
+# print(1,end=' ')
 i=3
 while i <= n:
-    c=a+b
-    print(c, end=' ')    
-    a,b=b,c
+    a,b=b,a+b
     i += 1
+print(b, end=' ')
